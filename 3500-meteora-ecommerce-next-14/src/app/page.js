577bba3006793273e15dcef52f3783c27698d711
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-17 01:51:34 -0300

    feat: Criado a API Router para detalhes do produto.

--- 3500-meteora-ecommerce-next-14/src/app/page.js
+++ 3500-meteora-ecommerce-next-14/src/app/page.js
@@ -1,7 +1,6 @@
 import styles from "./page.module.css";
 import { Categorias } from "./components/Categorias";
 import { Produtos } from "./components/Produtos";
-import { getCategorias, getTodosProdutos } from "../lib/api";
 
 async function fetchProdutosApi() {
   const res = await fetch("http://localhost:3000/api/produtos");
